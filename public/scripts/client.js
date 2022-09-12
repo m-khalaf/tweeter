0aa43cb19e5ee599f7dc86ea7ee25aca134b7666
m-khalaf <mkhalaf2211@gmail.com>
2022-09-12 01:28:03 +0000

    Stop Cross Script and Fix validation errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,25 +1,32 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
 
-  const createTweetElement = function (tweets) {
-    let markup = `<article>
+
+const esc = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const createTweetElement = function (tweets) {
+  let markup = `<article>
     <header>
       <div>
-        <img src=${tweets.user.avatars}>
-        <p>${tweets.user.name}</p>
+        <img src=${esc(tweets.user.avatars)}>
+        <p>${esc(tweets.user.name)}</p>
       </div>
-      <p>${tweets.user.handle}</p>
+      <p>${esc(tweets.user.handle)}</p>
     </header>
     
-    <p>${tweets.content.text}</p>
+    <p>${esc(tweets.content.text)}</p>
     <hr>
     
     <footer>
-      <p>${tweets.created_at}</p>
+      <p>${esc(timeago.format(tweets.created_at))}</p>
       <div>
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -28,60 +35,65 @@ $(document).ready(function () {
     </footer>
     </article>`;
 
-    return markup;
+  return markup;
 
+}
+
+
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+const renderTweets = function (tweets) {
+  tweets.map(tweet => {
+    const $tweet = createTweetElement(tweet);
+    $('#tweet-container').append($tweet);
+  })
+
+}
+
+const loadTweets = function () {
+  $.get('/tweets').then((data) => {
+    $('#tweet-text').val('')
+    $('#tweet-container').empty();
+    renderTweets(data);
+  })
+}
+
+const errMsg = function(text){
+  if($(".error").css('display')==='block'){
+    return;
   }
+  $(".error>span").append(`${text}`)
+  $(".error").slideDown("slow");
+  $('.close').click(() => {
+    $(".error").slideUp("slow")
+    $(".error>span").empty();
+  })
+}
 
-
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  const renderTweets = function (tweets) {
-    tweets.map(tweet => {
-      const $tweet = createTweetElement(tweet);
-      $('#tweet-container').append($tweet);
-    })
-
-  }
-
-
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
-
+$(document).ready(function () {
+  loadTweets();
 
   const $form = $('#tweet-form');
   $form.submit(function (event) {
     event.preventDefault();
-    console.log('you have submitted the form');
-    let data =$(this).serialize()
-    $.post('/tweets',data);
-    console.log('you sent ',data);
+    const submitted_Data = $(this).find('#tweet-text').val();
+    if (submitted_Data === '') {
+      errMsg('you are submitting an empty tweet...try again')
+      
+    } else if (submitted_Data.length > 140) {
+      errMsg('you are exceeding the max chars...try again')
+
+    } else {
+      let data = $(this).serialize()
+      $.post('/tweets', data)
+        .then(() => {
+          $(".error").hide();
+          loadTweets()
+        });
+
+
+    }
 
   })
 
