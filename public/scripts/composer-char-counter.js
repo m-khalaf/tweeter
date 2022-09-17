dfe0c35fadfa209a87e8cf43164a4b9435b7d158
m-khalaf <mkhalaf2211@gmail.com>
2022-09-17 22:44:03 +0000

    Add scroll up button

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,33 @@
-$(document).ready(function() {
-  $('#tweet-text').on('input', function(e) {
-    let totalChar= $(`#${this.id}`).val().length;
-    $(`#${this.id}`).parent().find('.counter').val(140-totalChar);
-    let counter= $(`#${this.id}`).parent().find('.counter');
-    let counterValue =counter.val();
+$(document).ready(function () {
+  $('#tweet-text').on('input', function (e) {
+    let totalChar = $(`#${this.id}`).val().length;
+    $(`#${this.id}`).parent().find('.counter').val(140 - totalChar);
+    let counter = $(`#${this.id}`).parent().find('.counter');
+    let counterValue = counter.val();
 
-    if (counterValue <0){
+    if (counterValue < 0) {
       counter.addClass('red');
     } else {
       counter.removeClass('red');
     }
   })
+  $(window).scroll(function (e) {
+    let st = $(this).scrollTop();
+    console.log('scrolld')
+    console.log(st)
+    if (st > 200) {
+      $('#scroll-button').css('display', 'block');
+      $('#scroll-button').click(() => {
+        $('#tweet-text').focus();
+        $(this).scrollTop(0)
+
+      })
+    } else {
+      $('#scroll-button').css('display', 'none');
+
+    }
+
+  })
+
+
 });
