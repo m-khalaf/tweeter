deb655862c42cd696039e24331f7cccf72ce8d26
m-khalaf <mkhalaf2211@gmail.com>
2022-09-17 23:55:49 +0000

    Add comments and eslint

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,13 +6,13 @@
  */
 
 
-const esc = function (str) {
+const esc = function(str) { //function to prevent cross site scripting from users inputs
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
-const createTweetElement = function (tweets) {
+const createTweetElement = function(tweets) { //function to create dynamic html article from user tweet
   let markup = `<article>
     <header>
       <div>
@@ -37,75 +37,85 @@ const createTweetElement = function (tweets) {
 
   return markup;
 
-}
+};
 
 
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
   tweets.map(tweet => {
     const $tweet = createTweetElement(tweet);
     $('#tweet-container').append($tweet);
-  })
+  });
 
-}
+};
 
-const loadTweets = function () {
+//sends a get request to fetch the tweets
+//empties the value of the text container
+//empties the tweet container to remmove all the tweets
+//renders all the tweets again inclding the new tweet
+//resets the value of the counter
+const loadTweets = function() {
   $.get('/tweets').then((data) => {
-    $('#tweet-text').val('')
+    $('#tweet-text').val('');
     $('#tweet-container').empty();
     renderTweets(data);
-  })
-}
+    $('.counter').val(140);
+  });
+};
 
-const errMsg = function (text) {
+//function to display error to user
+//checks if the error is already displayed so to does nothing
+//appends the error text passed by the user
+//displays the error message to the user and if user clicks on the x button it slides it back up
+const errMsg = function(text) {
   if ($(".error").css('display') === 'block') {
     return;
   }
-  $(".error>span").append(`${text}`)
+  $(".error>span").append(`${text}`);
   $(".error").slideDown("slow");
   $('.close').click(() => {
-    $(".error").slideUp("slow")
+    $(".error").slideUp("slow");
     $(".error>span").empty();
-  })
-}
+  });
+};
 
-$(document).ready(function () {
-  loadTweets();
+$(document).ready(function() {//wait till the page is loaded before excuting any of the code
+  loadTweets(); //load all tweets stored in data base
 
   const $form = $('#tweet-form');
-  $form.submit(function (event) {
-    event.preventDefault();
-    const submitted_Data = $(this).find('#tweet-text').val();
+  $form.submit(function(event) {
+    event.preventDefault(); //prevents the form from submtting when clicked on the button
+    const submitted_Data = $(this).find('#tweet-text').val(); //grabs the number of charachters in the tex area
     if (submitted_Data === '') {
-      errMsg('you are submitting an empty tweet...try again')
+      errMsg('you are submitting an empty tweet...try again'); //if user submits empty tweet it calls error function and pass the error message
 
     } else if (submitted_Data.length > 140) {
-      errMsg('you are exceeding the max chars...try again')
+      errMsg('you are exceeding the max chars...try again'); //if user exceeds chars limit calls error message function
 
     } else {
-      let data = $(this).serialize()
-      $.post('/tweets', data)
+      let data = $(this).serialize(); //seralizes the date submitted by the form
+      $.post('/tweets', data) //sends a post request to the server
         .then(() => {
-          $(".error").hide();
-          loadTweets()
+          $(".error").hide();//  hides the error message if displayed
+          loadTweets(); //calls the load tweet function
         });
     }
-  })
+  });
 
   const writeNewTweet = $('.tweet-icon>.nav-tweet');
-  writeNewTweet.click(function (event) {
+  writeNewTweet.click(function(event) {
     event.stopPropagation();
-    if($('.new-tweet').css('display')==='flex'){
+    if ($('.new-tweet').css('display') === 'flex') {//slides up the new tweet area when clicked (Stretch)
       $('.new-tweet').slideUp('slow');
-    }else if ($('.new-tweet').css('display')==='none'){
+    } else if ($('.new-tweet').css('display') === 'none') {//slides down the new tweet area when clicked (Stretch)
       $('.new-tweet').slideDown('slow');
-      $('#tweet-text').focus();
+      $('#tweet-text').focus();// brings the cursor in the text area
 
     }
-  })
-})
+  });
+});
 
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,33 +1,36 @@
-$(document).ready(function () {
-  $('#tweet-text').on('input', function (e) {
-    let totalChar = $(`#${this.id}`).val().length;
-    $(`#${this.id}`).parent().find('.counter').val(140 - totalChar);
-    let counter = $(`#${this.id}`).parent().find('.counter');
-    let counterValue = counter.val();
+$(document).ready(function() {
+  $('#tweet-text').on('input', function(e) {//listens to input event whenever a user types something in the txt area
+    let totalChar = $(`#${this.id}`).val().length;//grabs the number of chars entered in the text area
+    $(`#${this.id}`).parent().find('.counter').val(140 - totalChar); //updates the value of the counter
+    let counter = $(`#${this.id}`).parent().find('.counter'); //grabs the counter
+    let counterValue = counter.val(); //grabs the value of the counter
 
     if (counterValue < 0) {
-      counter.addClass('red');
+      counter.addClass('red'); //if counter value goes below zero it adds a class that turns it red
     } else {
-      counter.removeClass('red');
+      counter.removeClass('red'); //removes the red class
     }
-  })
-  $(window).scroll(function (e) {
-    let st = $(this).scrollTop();
-    console.log('scrolld')
-    console.log(st)
+  });
+  $(window).scroll(function(e) { //listens to scroll event
+    let st = $(this).scrollTop(); //grabs the scroll value from the top
+    if (st > 45) {
+      $('nav>span').css('color', '#4056A1'); //switches the twitter logo to blue when scrolled down
+    }
     if (st > 200) {
-      $('#scroll-button').css('display', 'block');
-      $('#scroll-button').click(() => {
-        $('#tweet-text').focus();
-        $(this).scrollTop(0)
+      $('#scroll-button').css('display', 'block');//displays the scroll up button when scrolled down
+      $('#scroll-button').click(() => {//listen to button click event
+        $('#tweet-text').focus();//brings back the cursor to the text area
+        $(this).scrollTop(0); //brings the window all the way up
 
-      })
+      });
     } else {
-      $('#scroll-button').css('display', 'none');
-
+      $('#scroll-button').css('display', 'none');// hides the button when scrolled up
+    }
+    if (st < 45) {
+      $('nav>span').css('color', '#fff'); //turns the tweeter logo back to white when scrolled up
     }
 
-  })
+  });
 
 
 });
